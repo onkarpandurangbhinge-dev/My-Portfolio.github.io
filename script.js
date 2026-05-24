e50516c646b43c88564abd747a5637a58df6f27a
onkar bhinge <onkarpandurangbhinge@gmail.com>
2026-05-24 12:12:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-// Dark mode toggle
-// =======================
-// DARK MODE
-// =======================
-const darkToggle = document.getElementById("darkToggle");
+// script.js
 
-darkToggle.onclick = () => {
+// ================= DARK MODE =================
+
+const toggleBtn = document.getElementById("toggleBtn");
+
+toggleBtn.onclick = () => {
   document.body.classList.toggle("dark");
+
+  // Change icon
+  if(document.body.classList.contains("dark")){
+    toggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+  }
+  else{
+    toggleBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+  }
 };
 
-// =======================
-// POPUP ANIMATION
-// =======================
-const sections = document.querySelectorAll(".section");
+// ================= POPUP ANIMATION =================
+
+const sections = document.querySelectorAll(".hidden");
 
 window.addEventListener("scroll", () => {
 
