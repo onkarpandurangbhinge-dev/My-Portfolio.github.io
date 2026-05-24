cc6c7ab749187a72d6c91fe2d0e836b9c4ebb3ea
onkar bhinge <onkarpandurangbhinge@gmail.com>
2026-05-24 12:07:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 // Dark mode toggle
-document.getElementById("darkToggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-});
+// =======================
+// DARK MODE
+// =======================
+const darkToggle = document.getElementById("darkToggle");
 
-// Scroll animation
+darkToggle.onclick = () => {
+  document.body.classList.toggle("dark");
+};
+
+// =======================
+// POPUP ANIMATION
+// =======================
 const sections = document.querySelectorAll(".section");
 
 window.addEventListener("scroll", () => {
-  sections.forEach(sec => {
-    const pos = sec.getBoundingClientRect().top;
-    if (pos < window.innerHeight - 100) {
-      sec.style.opacity = 1;
-      sec.style.transform = "translateY(0)";
-    }
-  });
-});
 
-sections.forEach(sec => {
-  sec.style.opacity = 0;
-  sec.style.transform = "translateY(50px)";
-  sec.style.transition = "0.6s";
+  sections.forEach(section => {
+
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if(sectionTop < window.innerHeight - 100){
+      section.classList.add("show");
+    }
+
+  });
+
 });
